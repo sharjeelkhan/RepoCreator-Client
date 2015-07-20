73ad84a0f24853240bdda3cc8e947c3a76494813
Micah Zoltu <micah@zoltu.net>
2015-07-19 21:08:23 -0700

    Adds aurelia-computed and updates aurelia-bootstrapper.

--- dist/source/main.ts
+++ dist/source/main.ts
@@ -1,17 +1,8 @@
-import { ConventionalViewStrategy } from 'aurelia-templating';
-
-// temporary fix while waiting for https://github.com/aurelia/templating/issues/121
-ConventionalViewStrategy.convertModuleIdToViewUrl= function(moduleId: any) {
-	var id = (moduleId.endsWith('.js') || moduleId.endsWith('.ts'))
-		? moduleId.substring(0, moduleId.length - 3)
-		: moduleId;
-	return id + '.html';
-}
-
 export function configure(aurelia: any) {
 	aurelia.use
 		.standardConfiguration()
-		.developmentLogging();
+		.developmentLogging()
+		.plugin('aurelia-computed');
 
 	aurelia.start().then((x: any) => x.setRoot('source/app', document.body));
 }
