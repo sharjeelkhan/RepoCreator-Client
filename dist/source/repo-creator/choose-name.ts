9ade233d37008fabf8e73ee1e594ee3f361bbe5d
Stiger Dieu <StigerDieu@gmail.com>
2015-10-09 23:12:55 +0700

    add validation to choose repository name

--- dist/source/repo-creator/choose-name.ts
+++ dist/source/repo-creator/choose-name.ts
@@ -2,18 +2,26 @@ import { autoinject } from 'aurelia-dependency-injection';
 import { computedFrom } from 'aurelia-binding';
 import { Router } from 'aurelia-router';
 import { EventAggregator } from 'aurelia-event-aggregator';
+import { Validation } from 'aurelia-validation';
 
 @autoinject()
 export class ChooseName {
 	constructor(
 		private router: Router,
-		private eventAggregator: EventAggregator
-	) {}
+        private eventAggregator: EventAggregator,
+        validation: Validation
+        ) {
+
+        this.validation = validation.on(this)
+            .ensure('newRepoName')
+            .isNotEmpty();
+    }
 	
 	private templateOwner: string;
 	private templateName: string;
 
-	protected newRepoName: string = '';
+    protected newRepoName: string = '';
+    protected validation: Validation;
 
 	public activate(parameters: any) {
 		this.templateOwner = parameters.owner;
@@ -26,6 +34,11 @@ export class ChooseName {
 	}
 
 	protected createRepository = (): void => {
-		this.router.navigate(`replacements/${this.templateOwner}/${this.templateName}/${this.newRepoName}`);
+        this.validation.validate()
+            .then(() => {
+                this.router.navigate(`replacements/${this.templateOwner}/${this.templateName}/${this.newRepoName}`);
+            })
+            .catch((validationResult: any) => {
+            });
 	}
 }
