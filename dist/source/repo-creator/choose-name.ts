c09b5a53157decb4fdaedea0c0e6dc6de1a49b27
Stiger Dieu <StigerDieu@gmail.com>
2015-10-10 16:38:53 +0700

    replace space with tabs

--- dist/source/repo-creator/choose-name.ts
+++ dist/source/repo-creator/choose-name.ts
@@ -8,37 +8,37 @@ import { Validation } from 'aurelia-validation';
 export class ChooseName {
 	constructor(
 		private router: Router,
-        private eventAggregator: EventAggregator,
-        validation: Validation
-        ) {
+		private eventAggregator: EventAggregator,
+		validation: Validation
+	) {
 
-        this.validation = validation.on(this)
-            .ensure('newRepoName')
-            .isNotEmpty();
-    }
-	
+		this.validation = validation.on(this)
+			.ensure('newRepoName')
+			.isNotEmpty();
+	}
+
 	private templateOwner: string;
 	private templateName: string;
 
-    protected newRepoName: string = '';
-    protected validation: Validation;
+	protected newRepoName: string = '';
+	protected validation: Validation;
 
 	public activate(parameters: any) {
 		this.templateOwner = parameters.owner;
 		this.templateName = parameters.name;
 	}
-	
+
 	@computedFrom('newRepoName')
 	protected get inputValid() {
 		return !!this.newRepoName;
 	}
 
 	protected createRepository = (): void => {
-        this.validation.validate()
-            .then(() => {
-                this.router.navigate(`replacements/${this.templateOwner}/${this.templateName}/${this.newRepoName}`);
-            })
-            .catch((validationResult: any) => {
-            });
+		this.validation.validate()
+			.then(() => {
+				this.router.navigate(`replacements/${this.templateOwner}/${this.templateName}/${this.newRepoName}`);
+			})
+			.catch((validationResult: any) => {
+			});
 	}
 }
